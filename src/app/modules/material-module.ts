ec484185d395ef1a3a69beebb1e866c1e3317356
jameseg <jamesellisgooding@gmail.com>
2022-07-19 17:30:43 -0400

    add styling to buttons

--- src/app/modules/material-module.ts
+++ src/app/modules/material-module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatExpansionModule } from '@angular/material/expansion';
 import { MatPaginatorModule } from '@angular/material/paginator';
@@ -7,6 +8,7 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
   exports: [
+    MatButtonModule,
     MatCardModule,
     MatExpansionModule,
     MatPaginatorModule,
